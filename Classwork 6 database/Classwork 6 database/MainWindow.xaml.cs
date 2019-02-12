b8f4bd56a8371adec5ec21592c49bc14d74c14ae
Ethan Seiber <ethcseib@ut.utm.edu>
2019-02-11 23:16:35 -0600

    Finished database

--- Classwork 6 database/Classwork 6 database/MainWindow.xaml.cs
+++ Classwork 6 database/Classwork 6 database/MainWindow.xaml.cs
@@ -25,24 +25,93 @@ namespace Classwork_6_database
 
         public MainWindow()
         {
-            cn = new OleDbConnection("Classwork_database1ConnectionString");
+            cn = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = |DataDirectory|Classwork database1.accdb");
             InitializeComponent();
         }
 
-        private void SeeAssets_Click(object sender, RoutedEventArgs e)
+        private void SeeAssets_Click_1(object sender, RoutedEventArgs e)
         {
             string query = "select* from Assets";
             OleDbCommand cmd = new OleDbCommand(query, cn);
+            
             cn.Open();
+            
             OleDbDataReader reader = cmd.ExecuteReader();
 
-            string data = "";
+            string Asset = "AssetID\n";
+            string Desc = "Description\n";
+            string EmployeeID = "EmployeeID\n";
 
             while (reader.Read())
             {
-                data += reader[0].ToString() + "\n";
+                Asset += reader[0].ToString() + "\n";
+                Desc += reader[1].ToString() + "\n";
+                EmployeeID += reader[2].ToString() + "\n";
             }
-            Text.Text = data;
+            TextBox1.Text = Asset;
+            TextBox2.Text = Desc;
+            TextBox3.Text = EmployeeID;
+
+            cn.Close();
+        }
+
+
+        private void SeeEmployees_Click(object sender, RoutedEventArgs e)
+        {
+
+            string query = "select* from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            cn.Open();
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            string ID = "EmployeeID\n";
+            string FirstName = "FirstName\n";
+            string LastName = "LastName\n";
+
+            while (reader.Read())
+            {
+                ID += reader[0].ToString() + "\n";
+                FirstName += reader[1].ToString() + "\n";
+                LastName += reader[2].ToString() + "\n";
+            }
+
+            TextBox4.Text = ID;
+            TextBox5.Text = FirstName;
+            TextBox6.Text = LastName;
+
+            cn.Close();
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox5_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox6_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
